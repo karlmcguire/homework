5cd612b1be31b0ea9c126b451a4cb794e4039376
Karl McGuire <karl@smonu.com>
2016-07-13 19:55:32 -0400

    progress

--- slcsp/main.go
+++ slcsp/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+func init() {
+	slcspFile, err := os.Open("slcsp.csv")
+	if err != nil {
+		panic(err)
+	}
+	slcsp := csv.NewReader(slcspFile)
+
+	plansFile, err := os.Open("plans.csv")
+	if err != nil {
+		panic(err)
+	}
+	plans := csv.NewReader(plansFile)
+
+	zipsFile, err := os.Open("zips.csv")
+	if err != nil {
+		panic(err)
+	}
+	zips := csv.NewReader(zipsFile)
+
+	fmt.Println(slcsp.Read())
+	fmt.Println(plans.Read())
+	fmt.Println(zips.Read())
+}
+
+func main() {
+
+}
