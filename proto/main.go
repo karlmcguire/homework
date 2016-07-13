e186c3e434740d4a3f4f108b0bef2b66cf1c059c
Karl McGuire <karl@smonu.com>
2016-07-13 18:57:39 -0400

    documentation

--- proto/main.go
+++ proto/main.go
@@ -22,45 +22,55 @@ const (
 	END
 )
 
+// getNumber is a helper function for converting binary big endian numbers to
+// the golang equivalent.
 func getNumber(slice []byte, out interface{}) error {
 	return binary.Read(bytes.NewReader(slice), binary.BigEndian, out)
 }
 
+// NewTransactionLog returns a TransactionLog after parsing a MPS7 transaction
+// log byte slice, b, with c being the amount of records.
 func NewTransactionLog(b []byte, c int) *TransactionLog {
 	var (
 		transactionLog = &TransactionLog{
 			Users: make(map[uint64]float64, 0),
 		}
 
-		iden   byte
-		user   uint64
-		amount float64
+		iden   byte    // current record identifier
+		user   uint64  // current record userId
+		amount float64 // current record amount (used if CREDIT || DEBIT)
 
-		i int
-		r int
+		i int // current byte index, i < len(b)
+		r int // current record index, i < c
 
 		err error
 	)
 
 	for r = 0; r < c; r++ {
+		// Determine current record type.
 		iden = b[i]
 		if iden > END {
 			panic(fmt.Errorf("invalid type at record %d\n", r))
 		}
 
+		// Determine if we need to get the userId and amount.
 		if iden == DEBIT || iden == CREDIT {
+			// Attempt to get the userId of current record.
 			if err = getNumber(b[i+5:i+13], &user); err != nil {
 				panic(fmt.Errorf("invalid userId binary at record %d\n", r))
 			}
 
+			// Attempt to get the amount of current record.
 			if err = getNumber(b[i+13:i+21], &amount); err != nil {
 				panic(fmt.Errorf("invalid amount binary at record %d\n", r))
 			}
 
 			if iden == DEBIT {
+				// Add amount to user's balance and debit total.
 				transactionLog.Users[user] += amount
 				transactionLog.DebitTotal += amount
 			} else {
+				// Subtract amount from user's balance, add to credit total.
 				transactionLog.Users[user] -= amount
 				transactionLog.CreditTotal += amount
 			}
@@ -95,6 +105,7 @@ func main() {
 		panic("version != 1")
 	}
 
+	// Determine the total amount of records contained in the file.
 	var c uint32
 	if err = getNumber(b[5:9], &c); err != nil {
 		panic("invalid record count")
