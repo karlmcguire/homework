e6f56067c2e5ada52ad2e10b07a408b7b846a03e
Karl McGuire <karl@smonu.com>
2016-07-13 18:26:11 -0400

    awesome

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	//"bytes"
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	//"sort"
 	"net/http/httptest"
+	"sort"
 	"strconv"
 )
 
 const (
-	crlf       = "\r\n"
-	colonspace = ": "
+	CRLF            = "\r\n"
+	COLONSPACE      = ": "
+	SEMICOLON       = ";"
+	CHECKSUMHEADERS = "X-Checksum-Headers"
+	CHECKSUM        = "X-Checksum"
 )
 
 func ChecksumMiddleware(h http.Handler) http.Handler {
@@ -21,15 +26,71 @@ func ChecksumMiddleware(h http.Handler) http.Handler {
 		rec := httptest.NewRecorder()
 		h.ServeHTTP(rec, r)
 
-		fmt.Println(w.Header())
-		fmt.Println(w.Status)
-		w.Write([]byte("test"))
+		// sortedKeys stores rec.Header() keys in lexicographic order.
+		sortedKeys := make([]string, 0)
+		for k, v := range rec.Header() {
+			w.Header()[k] = v
+			sortedKeys = append(sortedKeys, k)
+		}
+		sort.Strings(sortedKeys)
+
+		// canonResponse is the main buffer from which the checksum will be
+		// generated.
+		var canonResponse bytes.Buffer
+
+		// canonResponse = "418\r\n"
+		canonResponse.WriteString(strconv.Itoa(rec.Code))
+		canonResponse.WriteString(CRLF)
+
+		// checksumHeaders is the X-Checksum-Headers buffer which will be
+		// appended to canonResponse before calculating the checksum.
+		var checksumHeaders bytes.Buffer
+
+		// checksumHeaders = "X-Checksum-Headers: "
+		checksumHeaders.WriteString(CHECKSUMHEADERS)
+		checksumHeaders.WriteString(COLONSPACE)
+
+		// Iterate through rec.Header() in lexicographic order.
+		for _, headerKey := range sortedKeys {
+			// canonResponse + "Key: Value\r\n"
+			canonResponse.WriteString(headerKey)
+			canonResponse.WriteString(COLONSPACE)
+			canonResponse.WriteString(rec.Header().Get(headerKey))
+			canonResponse.WriteString(CRLF)
+
+			// checksumHeaders + "Key;"
+			checksumHeaders.WriteString(headerKey)
+			checksumHeaders.WriteString(SEMICOLON)
+		}
+
+		// X-Checksum-Headers can't end with a semicolon, remove it.
+		checksumHeaders.Truncate(checksumHeaders.Len() - 1)
+
+		// checksumHeaders + "\r\n\r\n"
+		checksumHeaders.WriteString(CRLF)
+		checksumHeaders.WriteString(CRLF)
+
+		// canonResponse + "X-Checksum-Headers: Key1;Key2;Key3;...\r\n"
+		canonResponse.Write(checksumHeaders.Bytes())
+
+		// canonResponse + content
+		canonResponse.Write(rec.Body.Bytes())
+
+		// Calculate checksum from canonResponse.
+		hash := sha1.Sum(canonResponse.Bytes())
+		checksum := hex.EncodeToString(hash[:])
+
+		// Add X-Checksum header.
+		w.Header().Set(CHECKSUM, checksum)
+
+		w.WriteHeader(rec.Code)
+		w.Write(rec.Body.Bytes())
 	})
 }
 
 // Do not change this function.
 func main() {
-	var listenAddr = flag.String("http", ":8080", "address to listen on for HTTP")
+	var listenAddr = flag.String("http", ":8080", "addcanonResponses to listen on for HTTP")
 	flag.Parse()
 
 	http.Handle("/", ChecksumMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
